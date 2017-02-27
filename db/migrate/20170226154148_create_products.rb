f06e4e22a6a5f9742ccfb16456dd936021a88dc2
Kendy <kenobi82403@gmail.com>
2017-02-27 22:10:56 +0000

    revert migration for products back to original file and create a new migration to add new columns and change column type

--- db/migrate/20170226154148_create_products.rb
+++ db/migrate/20170226154148_create_products.rb
@@ -1,7 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
-      t.string :brand, :title, :barcode_link, :ASIN, :image_url, :pdf_url, :EAN, :UPC, :model, :name
+      t.string :brand, :title, :barcode_link
+      t.integer :EAN, :UPC, :ASIN, :model
       t.text :description
       t.boolean :has_ar, default: false
       t.timestamps
