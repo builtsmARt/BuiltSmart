bdd671ee6379084b1c155170ad188cea35b610de
Leila Gleich <email": null>
2017-02-27 12:40:08 -0500

    Adjust show page to include extra product details

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 kane = User.create(full_name: "Kane", email: "k@k.com", password: "a")
 tony = User.create(full_name:"Tony", email: "t@t.com", password: "a")
 bob = User.create(full_name:"Bob", email: "b@b.com", password: "a")
-chair = Product.create(title:"chair")
+chair = Product.create(title:"Invisible Chair")
 tony.bookmarks.create(product_id: 1)
 tony.reviews.create(review:"This was a very difficult product to build. I sure wish I had been able to use BuiltSmart when I was putting it together! It broke but I had lost the manual and thought I would have to throw it away. Thanks to BuiltSmart I found the materials I need it to fix up my favorite chair!", difficulty_rating: "5", product_id: 1)
 
