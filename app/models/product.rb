1ff6e2209752253d2779fca25c761a91a120a4e0
Leila Gleich <email": null>
2017-02-28 11:36:59 -0500

    Write seeds to combine product types

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,22 @@ class Product < ApplicationRecord
   has_many :users, through: :bookmarks
   has_many :reviewers, through: :reviews, foreign_key: :user_id
 
-  #connect barcode link with EAN?
+  def upc_api(query)
+    root="https://api.upcitemdb.com/prod/trial/lookup?upc=#{query}"
+    response = Net::HTTP.get(URI(root))
+    @parsed_response = JSON.parse(response)["items"][0]
+    # @item_info={results: @parsed_response}
+    # @product = Product.new(title: parsed_response["title"])
+  end
+
+  def create_upc_product(upc)
+    self.upc_api(upc)
+    self.brand = @parsed_response['brand']
+    self.title = @parsed_response['title']
+    self.EAN = @parsed_response['ean']
+    self.ASIN = @parsed_response['brand']
+    self.model = @parsed_response['model']
+    self.description = @parsed_response['description']
+    self.save
+  end
 end
